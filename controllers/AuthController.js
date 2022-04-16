26b82ad363fcf8278c8148c1ca0839bd72d4ff83
Bryan <bryannsanchez1234@gmail.com.com>
2022-04-16 12:46:23 -0700

    added authcontroller

---
+++ controllers/AuthController.js
@@ -0,0 +1,20 @@
+const { Owner } = require('../models')
+
+const Login = async (req, res) => {
+  try {
+  } catch (error) {
+    throw error
+  }
+}
+
+const Register = async (req, res) => {
+  try {
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  Login,
+  Register
+}
