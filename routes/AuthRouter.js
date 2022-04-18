a5f3e2f607fb10169658982cabb78cf5d338321d
Bryan <bryannsanchez1234@gmail.com.com>
2022-04-18 11:16:24 -0700

    fixed auth

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -4,6 +4,11 @@ const middleware = require('../middleware')
 
 Router.post('/login', controller.Login)
 Router.post('/register', controller.Register)
+Router.get('/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
 // router.put(
 //   '/profile',
 //   middleware.stripToken,
