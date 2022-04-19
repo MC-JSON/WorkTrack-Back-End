3bf9c335921594e0904abfa2899b15bb6a19bfb7
Jason McCoy <jwmccoy29@gmail.com>
2022-04-19 13:29:04 -0500

    we are pulling

--- routes/OwnerRouter.js
+++ routes/OwnerRouter.js
@@ -2,16 +2,18 @@ const Router = require('express').Router()
 const controller = require('../controllers/OwnerController')
 const middleware = require('../middleware')
 
-Router.get('/:owner_id',
+Router.get(
+  '/:owner_id',
   middleware.stripToken,
   middleware.verifyToken,
-  controller.GetOwner)
+  controller.GetOwner
+)
 Router.get('/', controller.GetOwners)
-Router.get('/:owner_id/businesses',
-  middleware.stripToken,
-  middleware.verifyToken,
+Router.get(
+  '/:owner_id/businesses',
+  // middleware.stripToken,
+  // middleware.verifyToken,
   controller.GetOwnerBusinesses
-
 )
 // Router.post('/auth/register', controller.CreateOwner)
 Router.put('/:owner_id', controller.UpdateOwner)
